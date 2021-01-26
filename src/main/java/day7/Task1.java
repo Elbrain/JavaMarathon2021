cfa28aaf347cdf8e626a2d35addfebba53c727e1
Alex <alexdolzenko@gmail.com>
2021-01-26 21:58:11 +0000

    7ое задание

--- src/main/java/day7/Task1.java
+++ src/main/java/day7/Task1.java
@@ -2,6 +2,8 @@ package day7;
 
 public class Task1 {
     public static void main(String[] args) {
-
+        Airplane airplaneOne = new Airplane("Boeing", 1990, 137.5, 6320.32);
+        Airplane airplaneTwo = new Airplane("Cesna", 1995, 237.5, 1320.35);
+        Airplane.compareAirplanes(airplaneOne, airplaneTwo);
     }
 }
--- src/main/java/day7/Task2.java
+++ src/main/java/day7/Task2.java
@@ -1,7 +1,21 @@
 package day7;
 
+import java.util.Arrays;
+import java.util.Random;
+
 public class Task2 {
     public static void main(String[] args) {
+        Random rnd = new Random();
+        Player[] players = new Player[6];
+        for (int i = 0; i < players.length; i++) {
+            players[i] = new Player(90 + rnd.nextInt(11));
+            System.out.print(players[i].getStamina() + ", ");
+        }
+        System.out.println();
 
+        while (players[0].getStamina() > 0) {
+            players[0].run();
+        }
+        Player.info();
     }
 }
