0f0b156916145ab3c2338d5a8e06c8352d38f8f0
Alex <alexdolzenko@gmail.com>
2021-02-02 23:29:48 +0000

    14ое задание.

--- src/main/java/day14/Task1.java
+++ src/main/java/day14/Task1.java
@@ -1,7 +1,34 @@
 package day14;
 
-public class Task1 {
-    public static void main(String[] args) {
+import java.io.*;
 
+public class Task1 {
+
+    public static void main(String[] args) {
+        File file = new File("test.txt");
+        printSumDigits(file);
+    }
+
+    public static void printSumDigits(File file) {
+        int sum = 0;
+        int counter = 0;
+        try (FileReader reader = new FileReader(file); BufferedReader bufferedReader = new BufferedReader(reader)) {
+            String line;
+            while ((line = bufferedReader.readLine()) != null) {
+                sum += Integer.parseInt(line);
+                counter++;
+            }
+        } catch (IOException e) {
+            System.err.println("Such file not found");
+            e.printStackTrace();
+        }
+        if (counter != 10) {
+            try {
+                throw new IOException("Некорректный входной файл");
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+        System.out.println(sum);
     }
 }
--- src/main/java/day14/Task2.java
+++ src/main/java/day14/Task2.java
@@ -1,7 +1,41 @@
 package day14;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.regex.Pattern;
+
 public class Task2 {
     public static void main(String[] args) {
+        File file = new File("people.txt");
+        System.out.println(parseFileToStringList(file));
+    }
 
+    public static List<String> parseFileToStringList(File file) {
+        List<String> people = new ArrayList<>();
+        try (FileReader reader = new FileReader(file); BufferedReader bufferedReader = new BufferedReader(reader)) {
+            String line;
+            while ((line = bufferedReader.readLine()) != null) {
+                checkAge(line);
+                people.add(line);
+            }
+        } catch (IOException e) {
+            System.err.println("Файл не найден");
+            people.clear();
+            e.printStackTrace();
+        } catch (MyException e) {
+            e.printStackTrace();
+            people.clear();
+        }
+        return people;
+    }
+
+    public static void checkAge(String str) throws MyException {
+        if (str.contains(" -")) {
+            throw new MyException("Некорректный входной файл");
+        }
     }
 }
